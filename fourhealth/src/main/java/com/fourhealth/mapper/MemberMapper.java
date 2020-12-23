f3b5578e3de193fe2eda7e2fc9d62bc689a0ca06
DHM <gret8024@gmail.com>
2020-12-23 15:56:47 +0900

    Edit MemberMapper.java/MemberService.java with contents
    최초 커밋 후, 오류 발생으로 커밋 수정 및 로그인 로직을 위한 파일 수정 및 추가
    
    * 변경 및 추가 사항 상세 *
    - 로그인(회원/트레이너)
    - 프로모션 등록시 필수 정보 누락시 알림 표시
    - 프로모션 등록
    - 프로모션 리스트 수정 작업 중
    
    * Details of additional things *
    - Login function(Member/Trainer)
    - When add promotion, if some information none, working alaram func
    - Add Promotion
    - Working on promotion list
    
    2020/12/23 WED From DHM

--- fourhealth/src/main/java/com/fourhealth/mapper/MemberMapper.java
+++ fourhealth/src/main/java/com/fourhealth/mapper/MemberMapper.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.fourhealth.dto.MemberDTO;
+
 
 @Mapper
 public interface MemberMapper {
 	
-	
+	public MemberDTO getMember(String userId);
 }
--- fourhealth/src/main/java/com/fourhealth/service/MemberService.java
+++ fourhealth/src/main/java/com/fourhealth/service/MemberService.java
@@ -8,10 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fourhealth.dto.MemberDTO;
+import com.fourhealth.mapper.MemberMapper;
+
 
 @Service
 @Transactional
 public class MemberService {
+	@Autowired
+    private MemberMapper memberMapper;
 
+    public MemberDTO getMember(String userId) {
+    	MemberDTO memberDto = memberMapper.getMember(userId);
+        System.out.println("맴버 서비스 ------------" + memberDto);
+        return memberDto;
+    }
 
 }
