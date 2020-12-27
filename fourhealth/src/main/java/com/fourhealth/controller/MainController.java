f4d224b170cab517986e752860430ca6f5391b0d
Jeongho Yun <wjdgh7587@gmail.com>
2020-12-27 23:42:11 +0900

    Edit main page path
    # main페이지 경로 수정 및 화면 연결
    
    2020/12/27/Sun From Jeongho Yun
    
    Co-Authored-By: Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>

--- fourhealth/src/main/java/com/fourhealth/controller/MainController.java
+++ fourhealth/src/main/java/com/fourhealth/controller/MainController.java
@@ -8,37 +8,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 	/* thymeleaf 페이지 테스트 */
+	// index page(main)
 	@GetMapping("/")
 	public String test(Model model) {
 		model.addAttribute("test", "Hello thymeleaf");
 		return "index";
 	}
 
-	// Matching page
-	@GetMapping("/match")
+	// Sns page
+	@GetMapping("/sns")
 	public String matching(Model model) {
-		model.addAttribute("title", "Fourhealth 매칭");
-		return "match";
+		model.addAttribute("title", "Fourhealth matching");
+		return "main_layout/sns";
 	}
 
 	// Contact page
 	@GetMapping("/contact")
 	public String contact(Model model) {
-		model.addAttribute("title", "Fourhealth 컨텍트");
-		return "contact";
+		model.addAttribute("title", "Fourhealth Contact");
+		return "main_layout/contact";
 	}
 
 	// About Page
 	@GetMapping("/about")
 	public String about(Model model) {
 		model.addAttribute("title", "Fourhealth About");
-		return "about";
-	}
-
-	@GetMapping("/information")
-	public String information(Model model) {
-		model.addAttribute("title", "Fourhealth Information");
-		return "info";
+		return "main_layout/about";
 	}
 
 }
