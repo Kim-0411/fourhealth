9b697178c1406cea795e79b3bd754a260bca463e
gret8024 <gret8024@gmail.com>
2021-01-04 17:32:54 +0900

    Edit 사용자 최초정보 체크 기능
    #사용자 최초정보 체크기능 컨트롤러 수정
    
    #사용자 최초정보 체크기능 htm 수정
    
    2021/01/04 Mon From DHM
    
    Co-Authored-By: gret <75412422+gret8024@users.noreply.github.com>

--- fourhealth/src/main/java/com/fourhealth/controller/MatchingController.java
+++ fourhealth/src/main/java/com/fourhealth/controller/MatchingController.java
@@ -9,7 +9,7 @@ import com.fourhealth.service.MatchingService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,22 +22,22 @@ public class MatchingController {
     @PostMapping("/promotionPaymentCheck")
     public String checkPromotionPayment(@RequestParam(name = "userId", required = false) String userId,
             @RequestParam(name = "promotionNoticeCode", required = false) String promotionNoticeCode,
-            HttpServletResponse response) throws IOException {
+            HttpServletResponse response, Model model) throws IOException {
 
         System.out.println(userId);
-        System.out.println(matchingService.checkPromotionPayment(userId));
-        // String re = matchingService.checkPromotionPayment(userId);
-        // int i = Integer.parseInt(re);
-        // if (i > 0) {
-        // return "main_layout/promotion/promotionPayment";
-        // } else {
-        // response.setContentType("text/html; charset=UTF-8");
-        // PrintWriter out = response.getWriter();
-        // out.println("<script>alert('정보를 확인해주세요.'); location.href='/';</script>");
-        // out.flush();
-        // return "/";
-        // }
-        return "index";
+        System.out.println(promotionNoticeCode);
+        String re = matchingService.checkPromotionPayment(userId);
+        int i = Integer.parseInt(re);
+        if (i > 0) {
+            model.addAttribute("promotionNoticeCode", promotionNoticeCode);
+            return "main_layout/promotion/promotionPayment";
+        } else {
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script>alert('정보를 확인해주세요.'); location.href='/';</script>");
+            out.flush();
+            return "/";
+        }
     }
 
 }
