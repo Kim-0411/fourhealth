3d3d24e5b392c640ac6b52ffef297fe45c23cd88
internal7 <internal_c@naver.com>
2021-01-13 16:37:09 +0900

    Edit MemberDto 수정, MemberMapper.xml 수정
    #Fourhealth 회원가입 은행정보, 계좌번호, 계좌주 API에 이미 추가 되어 있어 불필요하므로  일부분 삭제
    
    2021.01.13 Wed From JDS

--- fourhealth/src/main/java/com/fourhealth/dto/MemberDto.java
+++ fourhealth/src/main/java/com/fourhealth/dto/MemberDto.java
@@ -5,47 +5,20 @@ import java.util.List;
 //테이블명 : tb_common_user 회원 공통 관
 public class MemberDto {
 
-    private String memberId;			//회원 공통 관리 아이디
-    private String memberLevel;			//회원 공통 권한별 레벨
-    private String memberPw;			//회원 공통 비밀번호
-    private String memberName;			//회원 공통 이름
-    private String memberAddr;			//회원 공통 주소
-    private String memberNickname;		//회원 공통 닉네임
-    private String memberPhone;			//회원 공통 전화번호
-    private String memberEmail;			//회원 공통 이메일
-	private String memberBirth;			//회원 공통 생년월일
-    private String memberGender;		//회원 공통 성별
-    private String memberMainBank;		//회원 계좌 은행
-    private String memberBankNumber;	//회원 계좌 번호
-    private String memberBankMaster;	//회원 계좌주
-    private String memberRegDate;       //회원 공통 등록 일자
-    private UserDto userDto;			//UserDto 활용
-    private TrainerDto trainerDto;		//TrainerDto 활용
-    
-    
-    public TrainerDto getTrainerDto() {
-		return trainerDto;
-	}
+    private String memberId; // 회원 공통 관리 아이디
+    private String memberLevel; // 회원 공통 권한별 레벨
+    private String memberPw; // 회원 공통 비밀번호
+    private String memberName; // 회원 공통 이름
+    private String memberAddr; // 회원 공통 주소
+    private String memberNickname; // 회원 공통 닉네임
+    private String memberPhone; // 회원 공통 전화번호
+    private String memberEmail; // 회원 공통 이메일
+    private String memberBirth; // 회원 공통 생년월일
+    private String memberGender; // 회원 공통 성별
+    private String memberRegDate; // 회원 공통 등록 일자
+    private UserDto userDto; // UserDto 활용
+    private TrainerDto trainerDto; // TrainerDto 활용
 
-	public void setTrainerDto(TrainerDto trainerDto) {
-		this.trainerDto = trainerDto;
-	}
-
-	public String getMemberEmail() {
-    	return memberEmail;
-    }
-    
-    public void setMemberEmail(String memberEmail) {
-    	this.memberEmail = memberEmail;
-    }
-
-    public String getMemberPhone() {
-    	return memberPhone;
-    }
-    
-    public void setMemberPhone(String memberPhone) {
-    	this.memberPhone = memberPhone;
-    }
     public String getMemberId() {
         return memberId;
     }
@@ -54,20 +27,20 @@ public class MemberDto {
         this.memberId = memberId;
     }
 
-    public String getMemberPw() {
-        return memberPw;
-    }
-
-    public void setMemberPw(String memberPw) {
-        this.memberPw = memberPw;
-    }
-
     public String getMemberLevel() {
         return memberLevel;
     }
 
     public void setMemberLevel(String memberLevel) {
         this.memberLevel = memberLevel;
+    }
+
+    public String getMemberPw() {
+        return memberPw;
+    }
+
+    public void setMemberPw(String memberPw) {
+        this.memberPw = memberPw;
     }
 
     public String getMemberName() {
@@ -94,6 +67,22 @@ public class MemberDto {
         this.memberNickname = memberNickname;
     }
 
+    public String getMemberPhone() {
+        return memberPhone;
+    }
+
+    public void setMemberPhone(String memberPhone) {
+        this.memberPhone = memberPhone;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
     public String getMemberBirth() {
         return memberBirth;
     }
@@ -110,54 +99,37 @@ public class MemberDto {
         this.memberGender = memberGender;
     }
 
-    public String getMemberMainBank() {
-        return memberMainBank;
-    }
-
-    public void setMemberMainBank(String memberMainBank) {
-        this.memberMainBank = memberMainBank;
-    }
-
-    public String getMemberBankNumber() {
-        return memberBankNumber;
-    }
-
-    public void setMemberBankNumber(String memberBankNumber) {
-        this.memberBankNumber = memberBankNumber;
-    }
-
-    public String getMemberBankMaster() {
-        return memberBankMaster;
-    }
-
-    public void setMemberBankMaster(String memberBankMaster) {
-        this.memberBankMaster = memberBankMaster;
-    }
-
     public String getMemberRegDate() {
         return memberRegDate;
     }
 
     public void setMemberRegDate(String memberRegDate) {
         this.memberRegDate = memberRegDate;
-    }   
+    }
 
-	public UserDto getUserDto() {
-		return userDto;
-	}
+    public UserDto getUserDto() {
+        return userDto;
+    }
 
-	public void setUserDto(UserDto userDto) {
-		this.userDto = userDto;
-	}
+    public void setUserDto(UserDto userDto) {
+        this.userDto = userDto;
+    }
 
-	@Override
-	public String toString() {
-		return "MemberDto [memberId=" + memberId + ", memberLevel=" + memberLevel + ", memberPw=" + memberPw
-				+ ", memberName=" + memberName + ", memberAddr=" + memberAddr + ", memberNickname=" + memberNickname
-				+ ", memberPhone=" + memberPhone + ", memberEmail=" + memberEmail + ", memberBirth=" + memberBirth
-				+ ", memberGender=" + memberGender + ", memberMainBank=" + memberMainBank + ", memberBankNumber="
-				+ memberBankNumber + ", memberBankMaster=" + memberBankMaster + ", memberRegDate=" + memberRegDate
-				+ ", userDto=" + userDto + ", trainerDto=" + trainerDto + "]";
-	}
+    public TrainerDto getTrainerDto() {
+        return trainerDto;
+    }
+
+    public void setTrainerDto(TrainerDto trainerDto) {
+        this.trainerDto = trainerDto;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDto [memberAddr=" + memberAddr + ", memberBirth=" + memberBirth + ", memberEmail=" + memberEmail
+                + ", memberGender=" + memberGender + ", memberId=" + memberId + ", memberLevel=" + memberLevel
+                + ", memberName=" + memberName + ", memberNickname=" + memberNickname + ", memberPhone=" + memberPhone
+                + ", memberPw=" + memberPw + ", memberRegDate=" + memberRegDate + ", trainerDto=" + trainerDto
+                + ", userDto=" + userDto + "]";
+    }
 
 }
