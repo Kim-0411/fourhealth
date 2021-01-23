9007c2c00781732084fed69ec295ba84efd70327
Jeongho Yun <wjdgh7587@gmail.com>
2021-01-23 13:05:56 +0900

    Update PaymentController.java
    # Updating serveral code
    
    2021/01/23 Sat From JHY

--- fourhealth/src/main/java/com/fourhealth/controller/PaymentController.java
+++ fourhealth/src/main/java/com/fourhealth/controller/PaymentController.java
@@ -2,10 +2,14 @@ package com.fourhealth.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import com.fourhealth.dto.NoticePromotionTrainerDto;
+import com.fourhealth.mapper.PaymentMapper;
+import com.fourhealth.mapper.PromotionMapper;
+import com.fourhealth.service.MemberService;
 import com.fourhealth.service.PaymentService;
 import com.fourhealth.service.PromotionService;
 
@@ -14,7 +18,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PaymentController {
