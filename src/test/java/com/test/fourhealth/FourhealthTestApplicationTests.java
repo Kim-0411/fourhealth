dc9dc7268abfbd98755ab8340b889c7e7e1891fb
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2021-07-13 15:41:54 +0900

    Update All files

---
+++ src/test/java/com/test/fourhealth/FourhealthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.fourhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourhealthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
